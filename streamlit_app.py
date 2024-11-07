783d0db9ca037017cee13108fde9527b99860348
Graham Stott <37821655+GrahamStott18@users.noreply.github.com>
2024-11-07 09:26:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -46,7 +47,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order, icon="✅")
     
 # New section to display fruityvice nutrition information
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)

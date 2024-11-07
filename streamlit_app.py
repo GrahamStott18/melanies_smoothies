165dd0e249de028b37682edd2c7b4a3606808c96
Graham Stott <37821655+GrahamStott18@users.noreply.github.com>
2024-11-07 09:29:28 -0600

    Update streamlit_app.py
    commented out issues until fix provided

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -48,6 +47,6 @@ if ingredients_list:
     
 # New section to display fruityvice nutrition information
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+# fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
